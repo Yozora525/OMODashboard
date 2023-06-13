5691b6af5fda05bf459212e7333c599fe3403839
SHI-Yan LEE <69109740+Yozora525@users.noreply.github.com>
2023-06-13 22:16:29 +0800

    20230613--shiyan 1. add     * frontend-completed draw chart function, but I haven't test. 2. modify     * frontend-delete not used code. 3. push current progress

--- static/js/commonTools.js
+++ static/js/commonTools.js
@@ -22,24 +22,96 @@ function AjaxRequest(url,params,successFunc,errorFunc) {
 }
 
 // draw chart
-function DrawChart(chartType,canvasElement,xValue,yValue,datasets){
-    // check xValue length and yValue length
-    if(xValue.length != yValue.length){ 
-        alert('x軸與y軸長度不一致！');
-        return;
-    }
+function SetMultipleChart(CanvasElement,xValues,Datasets, yMaxValue, ChartType,ChartOptionsKey,ChartOptionsValue,onClickFunc,onHoverFunc, yShowPercentage, yBeginAtZero,yScaleColors,yScaleDrawBorder=false) {
+    isNaN(parseInt(yMaxValue)) ? yMaxValue = 10 : false;
+    isNaN(parseInt(yShowPercentage)) ? yShowPercentage = 0 : yShowPercentage = 1;
+    isNaN(parseInt(yBeginAtZero)) ? yBeginAtZero = 1 : yBeginAtZero = 0;
+
+    ChartType = ChartType || $(CanvasElement).attr("data-chart-type");
 
     let ChartsConfigData = {
-        type: chartType,
+        type: ChartType,
         data: {
-            labels: xValue,
-            datasets: datasets
+            labels: xValues,
+            datasets: Datasets
         }
     }
 
+    switch (ChartType.toLowerCase()) {
 
+        case 'doughnut':
+            ChartsConfigData['options'] = {
+                title: {
+                    display: true,
+                    text: ""
+                }
+            }
+            break;
 
-    new Chart($(canvasElement), ChartsConfigData);
+        case 'bar':
+            ChartsConfigData['options'] = {
+                scales: {
+                    y: {
+                        beginAtZero: yBeginAtZero,
+                        grid: {
+                            color: yScaleColors ,
+                            drawBorder: yScaleDrawBorder
+                            /*y 軸線顏色*/
+                        }
+                    },
+                    x: {
+                        grid: {
+                            display: false,
+                        }
+                    }
+                },
+                legend: {
+                    display: 0,
+                }
+            }
+            break;
+    
 
-    return $(canvasElement);
+        default:
+            ChartsConfigData['options']= {
+                legend: {
+                    display: 0
+                },
+                title: {
+                    display: 1,
+                    text: ""
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: yBeginAtZero,
+                            suggestedMax: yMaxValue,
+                            callback: function (value, index, values) {
+                                if (yShowPercentage){
+                                    return Math.floor(value) + "%";
+                                } else {
+                                    return value;
+                                }
+                            }
+                        },
+                    }]
+                }
+            }
+    }
+
+    if (ChartOptionsKey.length != ChartOptionsValue.length) {
+        console.log('ChartOptionsKey與ChartOptionsValue長度不一致！');
+        return;
+    } else {
+        for (let i = 0; i < ChartOptionsKey.length; i++) {
+            ChartsConfigData['option'][ChartOptionsKey[i]] = ChartOptionsValue[i];
+        }
+    }
+
+    onClickFunc ? ChartsConfigData['options']['onClick'] = onClickFunc : onClickFunc;
+    onHoverFunc ? ChartsConfigData['options']['onHover'] = onHoverFunc : onHoverFunc;
+
+    new Chart($(CanvasElement), ChartsConfigData);
+
+    return $(CanvasElement);
 }
