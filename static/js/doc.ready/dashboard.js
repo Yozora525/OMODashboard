3c880fded933adf05de0702c9c60ea7c4bf0ea44
SHI-Yan LEE <69109740+Yozora525@users.noreply.github.com>
2023-06-15 08:28:02 +0800

    20230615--shiyan 1. add     * 前端-儀表板，新增業務員業績及目標圖表 2. push current progress

--- static/js/doc.ready/dashboard.js
+++ static/js/doc.ready/dashboard.js
@@ -16,8 +16,22 @@ $(function(){
         borderColor: '#124429',
     }],false,'bar',[['scales','y','grid','color']],['blue']);
 
-    // todo 業務員業績及目標 by shiyan
-
+    // 業務員業績及目標 by shiyan
+    SetMultipleChart("[data-chart='aim']",['業務員1','業務員2','業務員3','業務員4'],[{
+        type: 'line',
+        label: '當前業績',
+        data: [16500,23700,33000,30000],
+        backgroundColor: "blue",
+        borderWidth: 2,
+        borderColor: '#124429',
+    },{
+        type: 'bar',
+        label: '業績目標',
+        data: [28000,30000,31000,27000],
+        backgroundColor: "yellow",
+        borderWidth: 2,
+        borderColor: '#124429',
+    }],false,'bar',[['scales','y','grid','color']],['blue']);
 
     // todo 接觸點 by shiyan
 
